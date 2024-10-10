2d4932e07648fb86ec2885d9a6b8c5b6e1a5fbad
Alisson Barbosa <161394750+Alysson-b@users.noreply.github.com>
2024-10-09 21:42:49 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+base = float(input('Insira a base do triangulo em CM.'))
+altura = float(input('Insira a altura do triangulo CM'))
+area = base * altura 
+
+print(f'A área do retângulo é: {area:.2F}')
